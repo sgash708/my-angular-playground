54719383023ec0efd0ebe6eadabd9b0613fd03ca
sgash708 <sugash7081023@gmail.com>
2022-04-02 16:08:15 +0900

    feat: componentについて

---
+++ official-tutorial/basic/about_component/index.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  template: `
+    <h2>hello world</h2>
+  `
+})
+export class HelloWorldComponent {
+  title = 'hoge';
+}
