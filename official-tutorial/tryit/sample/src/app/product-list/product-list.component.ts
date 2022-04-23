594c210a95cb8c9bd78ee2cd9eac775067a7bef7
sgash708 <sugash7081023@gmail.com>
2022-04-23 13:43:50 +0900

    chore: component作成

---
+++ official-tutorial/tryit/sample/src/app/product-list/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+})
+export class ProjectListComponent {
+}
